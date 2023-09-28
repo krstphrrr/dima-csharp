7b42ff908d473ff36718a634834b2cdad9778975
Kris! <42949874+krstphrrr@users.noreply.github.com>
2023-09-27 23:44:39 -0600

    tested oledb connection

--- Dockerfile
+++ Dockerfile
@@ -2,10 +2,14 @@ FROM mcr.microsoft.com/dotnet/sdk:7.0 AS build
 WORKDIR /app
 RUN dotnet new console
 COPY ./src/test.cs Program.cs
+RUN dotnet add package System.Data.OleDb --version 7.0.0
 RUN dotnet publish -c Release -o out
+# CMD ["tail","-f", "/dev/null"]
 
 FROM mcr.microsoft.com/dotnet/runtime:7.0
 WORKDIR /app
+COPY ./external .
 COPY --from=build /app/out .
+
 ENTRYPOINT ["dotnet", "app.dll"]
 # CMD ["tail","-f", "/dev/null"]
--- src/test.cs
+++ src/test.cs
@@ -1,1 +1,38 @@
-Console.WriteLine("Hello World!");
+using System;
+using System.Data;
+using System.Data.OleDb;
+
+namespace DimaScript
+{
+  class Program
+  { 
+     static private string dimapath = "/app/external/external/NDOW VHA Public Lands DIMA 5.6 as of 2022.09.06_EditedCoords.mdb";
+     static void Main(){
+      // Program obj = new Program();
+      var resultSet = GetDBTables(dimapath);
+      Console.WriteLine(resultSet);
+    }
+    
+    
+    static public System.Collections.Generic.List<string> GetDBTables(string path)
+      {
+        System.Collections.Generic.List<string> allTables = new System.Collections.Generic.List<string>();
+        String connect = ("Microsoft.JET.OLEDB.4.0;data source=" 
+                          + path + ";Persist Security Info=False;");
+        System.Data.OleDb.OleDbConnection con = new System.Data.OleDb.OleDbConnection(connect);
+        con.Open();
+
+        System.Data.DataTable tables = con.GetOleDbSchemaTable(OleDbSchemaGuid.Tables,
+                                    new object[] {null, null, null, "TABLE"});
+        int counter = 1;
+        foreach (System.Data.DataRow row in tables.Rows)
+        {
+          allTables.Add(row[2].ToString());
+          counter++;
+        }
+        con.Close();
+        return allTables;
+      }
+  }
+}
+
